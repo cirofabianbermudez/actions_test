eb23ffda202b5806c09cefffcab9103cbdcdaf25
cirofabianbermudez <cirofabian.bermudez@gmail.com>
2024-03-04 20:10:59 +0100

    gitaction added and basic code

---
+++ codes/hello.cpp
@@ -0,0 +1,16 @@
+// Date:        2024/03/04
+// Author:      Ciro Fabian Bermudez Marquez
+// Description: Simple hello name program
+
+#include <iostrem>
+#include <string>
+
+// using namespace std;
+
+int main(int argc, char** argv) {
+  std::string name;
+
+  std::cin >> name;
+  std::cout << "Hello " << name << std::endl;
+  return 0;
+}
